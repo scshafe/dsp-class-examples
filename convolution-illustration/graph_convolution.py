33ad44e1691c2b86d886e5ba9fa40fb78e1bce76
scshafe <scshafe@umich.edu>
2025-01-04 02:39:58 -0800

    convolution works (assumes signals start at n=0)

--- convolution-illustration/graph_convolution.py
+++ convolution-illustration/graph_convolution.py
@@ -11,12 +11,6 @@ import matplotlib.pyplot as plt
 from matplotlib import animation
 
 
-def conv_index(sequence, n, k):
-    # return s[n-k] or 0 if out of index
-    if n-k < 0 or n-k >= len(sequence):
-        return 0
-    return sequence[n-k]
-
 '''
 This function creates the convolution:
 
@@ -30,14 +24,19 @@ output sequence will be length of s1
 the "impulse response" (aka s2) is padded with 0's
 '''
 def create_convolution(s1, s2):
-    output = np.zeroes(len(s1))
+    output = np.zeros(len(s1))
+    print(s2)
     for n in range(len(s1)):
         inner_product = 0
         # tmp is the inverted, shifted impulse response h[n-k]
-        for k in range(len(s1)):
-            inner_product += s1[k] * conv_index(s2, n, k)
-        output[n] = inner_product
+        tmp = np.array(s2[n::-1])
+        tmp = np.pad(tmp, [0, len(s1)-len(tmp)])
+        inner_product = s1 * tmp
+
+
+        output[n] = np.sum(inner_product)
     return output
+
 
 
 def create_convolution_algorithm_animation(s1, s2):
@@ -51,25 +50,28 @@ def create_convolution_algorithm_animation(s1, s2):
 
 
 
-sequence_lengths=10
+sequence_lengths=20
 
-# input sequence length 10
-input_sequence = np.array([0,0,0,3,5,0,2,0,0,0])
+input_unpadded = np.array([0,0,0,2,3,1])
+input_sequence = np.pad(input_unpadded, [0, sequence_lengths - len(input_unpadded)])
+# print(input_sequence)
 assert(len(input_sequence) == sequence_lengths)
 
 # impulse response is exponential decay
 alpha = 0.9
-impulse_response = np.array(alpha**x for x in range(sequence_lengths))
+impulse_response = np.array([(alpha**x) for x in range(sequence_lengths)])
+# print(impulse_response)
 
+conv = create_convolution(input_sequence, impulse_response)
 
 print(impulse_response)
 
 plt.figure(figsize=(9,7))
 plt.subplot(311)
-plt.stem([i for i in range(10)], input_sequence, basefmt='b')
+plt.stem([i for i in range(sequence_lengths)], input_sequence, basefmt='b')
 plt.subplot(312)
-plt.stem([i for i in range(10)], input_sequence, basefmt='b')
+plt.stem([i for i in range(sequence_lengths)], input_sequence, basefmt='b')
 plt.subplot(313)
-plt.stem([i for i in range(10)], input_sequence, basefmt='b')
+plt.stem([i for i in range(sequence_lengths)], conv, basefmt='b')
 
 plt.show()
