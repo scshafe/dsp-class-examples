f8b2be4623a3e101339b7649c7b099b2448f3e40
scshafe <scshafe@umich.edu>
2025-01-12 15:13:42 -0800

    animation is working

--- convolution-illustration/graph_convolution.py
+++ convolution-illustration/graph_convolution.py
@@ -41,28 +41,34 @@ def create_convolution(s1, s2):
 
 sequence_lengths=20
 input_unpadded = np.array([1,2,3])
-input_sequence = np.pad(input_unpadded, [0, sequence_lengths - len(input_unpadded)])
+# input_sequence = np.pad(input_unpadded, [0, sequence_lengths - len(input_unpadded)])
+impulse_response = np.pad(input_unpadded, [0, sequence_lengths - len(input_unpadded)])
 
 alpha = 0.9
-impulse_response = np.array([(alpha**x) for x in range(sequence_lengths)])
+# impulse_response = np.array([(alpha**x) for x in range(sequence_lengths)])
+input_sequence = np.array([(alpha**x) for x in range(sequence_lengths)])
 
 x = np.array([i for i in range(sequence_lengths)])
 
 output = np.zeros(sequence_lengths)
 
+empty = np.array([0 for i in range(sequence_lengths)])
+
 
 # Create figure and subplots
 fig, (ax1, ax2, ax3) = plt.subplots(3, 1, figsize=(8, 6))
 
-
+markerline1, stemlines1, baseline1 = ax1.stem(x, input_sequence, '-.')
+markerline2, stemlines2, baseline2 = ax2.stem(x, output, '-.')
+markerline3, stemlines3, baseline3 = ax3.stem(x, output, '-.')
 # Line objects for each plot
-ax1.stem(x, input_sequence)
-ax2.stem(x, impulse_response)
-ax3.stem(x, output)
+# ax1.stem(x, input_sequence)
+# ax2.stem(x, impulse_response)
+# ax3.stem(x, output)
 
-line1 = ax1.stem(x, input_sequence)
-line2 = ax2.stem(x, impulse_response)
-line3 = ax3.stem(x, output)
+# line1 = ax1.stem(x, input_sequence)
+# line2 = ax2.stem(x, impulse_response)
+# line3 = ax3.stem(x, output)
 
 # Animation update function
 # def animate(i):
@@ -78,7 +84,11 @@ line3 = ax3.stem(x, output)
 
 
 def init():
-    return line1, line2, line3
+    markerline1, stemlines1, baseline1 = ax1.stem(x, empty, '-.')
+    markerline2, stemlines2, baseline2 = ax2.stem(x, empty, '-.')
+    markerline3, stemlines3, baseline3 = ax3.stem(x, empty, '-.')
+    return markerline1, stemlines1, baseline1, markerline2, stemlines2, baseline2, markerline3, stemlines3, baseline3
+    # return line1, line2, line3
 
 '''
 Function to call for each frame of the animation. There are 3 plots:
@@ -89,6 +99,10 @@ Function to call for each frame of the animation. There are 3 plots:
 def create_convolution_algorithm_animation(frame_num):
     print(frame_num)
 
+    ax1.cla()
+    ax2.cla()
+    ax3.cla()
+
     tmp = np.array(impulse_response[frame_num::-1])
     tmp = np.pad(tmp, [0, len(input_sequence) - len(tmp)])
 
@@ -97,17 +111,23 @@ def create_convolution_algorithm_animation(frame_num):
     print(inner_product)
     output[frame_num] = inner_product
 
-    line1.set_data(x, input_sequence)
-    line2.set_data(x, tmp)
-    line3.set_data(x, output)
 
-    return line1, line2, line3
-    # line2.set_ydata(tmp)
-    # line3.set_ydata(output)
+    # markerline1, stemlines1, baseline1 = ax1.stem(x, input_sequence, '-.')
+    # markerline2, stemlines2, baseline2 = ax2.stem(x, tmp, '-.')
+    # markerline3, stemlines3, baseline3 = ax3.stem(x, output, '-.')
+
+    markerline1, stemlines1, baseline1 = ax1.stem(x, input_sequence, '-.')
+    markerline2, stemlines2, baseline2 = ax2.stem(x, tmp, '-.')
+    markerline3, stemlines3, baseline3 = ax3.stem(x, output, '-.')
+    # return line1, line2, line3
+    return stemlines1, stemlines2, stemlines3
+
 
 
 # anim = FuncAnimation(fig, create_convolution_algorithm_animation, #init_func = init, 
 #                      frames = 20, interval = 1, blit = True) 
 
-anim = FuncAnimation(fig, create_convolution_algorithm_animation,
-                     frames = 20, interval = 1, blit = True) 
+anim = FuncAnimation(fig, create_convolution_algorithm_animation, 
+                     frames = 20, interval = 200) 
+
+anim.save("convolution.gif")
